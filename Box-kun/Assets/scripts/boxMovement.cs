e06c46bcd56396aa10f5e74268589f75634e2aa6
byjuw <julien.wachter@gmail.com>
2017-02-21 21:15:49 +0100

    Add basics moves
    Basics moves added, rotation need to be fixed.

---
+++ Box-kun/Assets/scripts/boxMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxMovement : MonoBehaviour
+{
+	public float acceleration_x = 10f;
+	public float speed = 10f;
+	private bool grounded = true;
+	private float jump_pressure = 0f;
+	private float jump_pressure_max = 10f;
+	private float jump_min = 2f;
+	private Rigidbody2D rb;
+
+	// tags that can destroy the sub
+	public string[] destroyers;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		rb.velocity = new Vector2 (speed + Input.GetAxis ("Horizontal") * acceleration_x, rb.velocity.y);
+		transform.Rotate(0, 0, -Input.GetAxis ("Horizontal")*10);
+		if (grounded) {
+			//Chargement jump
+			if (Input.GetButton ("Jump")) {
+				if (jump_pressure < jump_pressure_max) {
+					jump_pressure += Time.deltaTime * 10f;
+				}
+				else {
+					jump_pressure = jump_pressure_max;
+				}
+			}
+			else {
+				if (jump_pressure > 0f) {
+					jump_pressure += jump_min;
+					rb.velocity = new Vector2 (rb.velocity.x, jump_pressure);
+					jump_pressure = 0f;
+					grounded = false;
+				}
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "ground") {
+			grounded = true;
+		}
+	}
+}
---
+++ Box-kun/Assets/scripts/cameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+	public Transform lookAt;
+	private Vector3 offset = new Vector3 (0, 0, -10f);
+
+	void LateUpdate()
+	{
+		Vector3 desiredPosition = lookAt.transform.position + offset;
+		transform.position = desiredPosition;
+	}
+}
