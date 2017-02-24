4521068f139ae53d9b2ef3a654447685c960ee17
HappyTetris <giribaldiraphael@gmail.com>
2017-02-24 11:00:08 +0100

    Box Movement

--- Box-kun/Assets/scripts/boxMovement.cs
+++ Box-kun/Assets/scripts/boxMovement.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class boxMovement : MonoBehaviour
 {
 	public float acceleration_x = 10f;
-	public float speed = 13f;
-	private bool grounded = true;
+	private float accelerationJump = 15f;
+	public float speed = 10f;
+	public float speed2 = -10f;
+	private bool grounded = false;
+	private bool grounded2 = false;
+	private bool jumping = false;
+	private bool jumping2 = false;
 	public float jump_pressure = 25f;
 	public float jump_pressure_max = 25f;
 	public float jump_min = 4f;
@@ -28,7 +33,18 @@ public class boxMovement : MonoBehaviour
 		goRot = Quaternion.Angle (Quaternion.Euler (new Vector3 (0, 0, 0)), transform.rotation);
 		goScale = transform.localScale.y;
 		//Horizontal Movement
-		rb.velocity = new Vector2 (speed + Input.GetAxis ("Horizontal") * acceleration_x, rb.velocity.y);
+		if (grounded && !jumping && !jumping2) {
+			rb.velocity = new Vector2 (speed + Input.GetAxis ("Horizontal") * acceleration_x, rb.velocity.y);
+		} else if (grounded2 && !jumping && !jumping2) {
+			rb.velocity = new Vector2 (speed2 + Input.GetAxis ("Horizontal") * acceleration_x, rb.velocity.y);
+		} else if (jumping) {
+			rb.velocity = new Vector2 (speed/5 +  Input.GetAxis ("Horizontal") * accelerationJump, rb.velocity.y);
+		} else if (jumping2) {
+			rb.velocity = new Vector2 (speed2/5  + Input.GetAxis ("Horizontal") * accelerationJump, rb.velocity.y);
+		}
+		print (grounded);
+		print (grounded2);
+
 		//Rotation
 		transform.Rotate (0, 0, -Input.GetAxis ("Horizontal") * 10);
 
@@ -37,26 +53,18 @@ public class boxMovement : MonoBehaviour
 			//Chargement jump
 			if (Input.GetButton ("Jump")) {
 
-				rb.velocity = new Vector2 (rb.velocity.x, jump_pressure);
-				grounded = false;
-//				if (jump_pressure < jump_pressure_max) {
-//					jump_pressure += Time.deltaTime * 5 * 10f;
-//
-//
-//				}
-//				else {
-//					jump_pressure = jump_pressure_max;
-//				}
+				rb.velocity = new Vector2 (speed + rb.velocity.x, jump_pressure);
+				jumping = true;
+
 			}
-//			else {
-//				if (jump_pressure > 0f) {
-//					jump_pressure += jump_min;
-//					rb.velocity = new Vector2 (rb.velocity.x, jump_pressure);
-//					jump_pressure = 0f;
-//					grounded = false;
-//				}
-//			}
+
+		} else if (grounded2 && (goRot < 80 && goRot > -80)) {
+			if (Input.GetButton ("Jump")) {
+				rb.velocity = new Vector2 (speed2 + rb.velocity.x, jump_pressure);
+				jumping2 = true;
+			}
 		}
+		
 	}
 
 	//Detection ground
@@ -64,6 +72,23 @@ public class boxMovement : MonoBehaviour
 	{
 		if (coll.gameObject.tag == "ground") {
 			grounded = true;
+			jumping = false;
+			jumping2 = false;
+		}
+		if (coll.gameObject.tag == "ground2") {
+			grounded2 = true;
+			jumping = false;
+			jumping2 = false;
 		}
 	}
+	void OnCollisionExit2D(Collision2D collLeave) 
+	{
+		if (collLeave.gameObject.tag == "ground") {
+			grounded = false;
+		}
+		if (collLeave.gameObject.tag == "ground2") {
+			grounded2 = false;
+		}
+	}
+		
 }
