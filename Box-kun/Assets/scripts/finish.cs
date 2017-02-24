a50465093f7803672036c7a7a0ed939a275b10e9
byjuw <julien.wachter@gmail.com>
2017-02-24 11:23:02 +0100

    Add finish lvl + particules on water

---
+++ Box-kun/Assets/scripts/finish.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finish : MonoBehaviour {
+	private Animation animation;
+
+	// Use this for initialization
+	void Start () {
+		animation = GetComponent<Animation>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			animation.Play();
+		}
+	}
+}
